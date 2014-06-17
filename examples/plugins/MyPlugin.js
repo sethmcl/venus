3938b0761f513807cd2e3ffc331fedd687363e70
Seth McLaughlin <s@sethmcl.com>
2014-06-17 09:59:00 -0700

    WIP

---
+++ examples/plugins/MyPlugin.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = MyPlugin;
+
+/**
+ * @constructor
+ * @param {object} ctx - VenusCtx object
+ */
+function MyPlugin(ctx) {
+
+}
+
+MyPlugin.prototype.init = function () {
+
+};
+
+MyPlugin.prototype.attach = function () {
+
+};
+
+MyPlugin.prototype.argv = function () {
+
+};
+
+MyPlugin.prototype.run = function () {
+
+};
--- test/unit/lib/config.spec.js
+++ test/unit/lib/config.spec.js
@@ -1,5 +1,23 @@
 var v      = require('../../helpers');
-var config = v.r('config');
+var Config = v.r('Config');
 
 describe('config', function () {
+  var config;
+
+  before(function () {
+    config = new Config();
+  });
+
+  it('should return config context', function (done) {
+    var testFile = v.path('fixtures', 'projects', 'project_a', 'test.spec.js');
+
+    config.ctx(testFile)
+      .then(function (ctx) {
+        v.assert.deepEqual(ctx.get('includeGroup'), ['normal']);
+        done();
+      })
+      .then(null, function (err) {
+        done(err);
+      });
+  });
 });
